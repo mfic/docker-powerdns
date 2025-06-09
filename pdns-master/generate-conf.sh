ae3d6ad2415322c6b3b3b229fa76f9b3f47edb91
Jan Quest <jan@questnet.eu>
2025-06-09 13:54:59 +0200

    clean up and update my.cnf

--- pdns-master/generate-conf.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-set -e
-
-# Load .env vars from all relevant sources
-export $(grep -v '^#' .env | xargs)
-
-# Generate sql.conf
-envsubst <./config/templates/sql.conf.template >./config/pdns.d/sql.conf
-envsubst <./config/templates/api.conf.template >./config/pdns.d/api.conf
